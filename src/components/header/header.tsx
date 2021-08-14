2133bbf1c15b8f2724cca66113a5bb9a2cc75bb9
Crepiks <sayazhan.onlassyn@mail.ru>
2021-08-14 22:13:24 +0600

    feat: create login form markup

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Button } from "antd";
 import styles from "./header.module.css";
+import { useHistory } from "react-router-dom";
 
 function Header() {
+  const history = useHistory();
+
+  function handleLoginButtonClick() {
+    history.push("/login");
+  }
+
   return (
     <div className={styles.header}>
       <span className={styles.logo}>BeeJee</span>
-      <Button type="primary">Login</Button>
+      <Button type="primary" onClick={handleLoginButtonClick}>
+        Login
+      </Button>
     </div>
   );
 }
--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import styles from "./login.module.css";
+import { useState } from "react";
 
 function LoginView() {
-  return <div>This is login view</div>;
+  const [loading, setLoading] = useState();
+
+  return (
+    <div className={styles.login}>
+      <div className={styles.loginForm}>
+        <h3 className={styles.title}>Login</h3>
+        <Form layout="vertical">
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input username",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input password" }]}
+          >
+            <Input />
+          </Form.Item>
+          <div className={styles.submitWrapper}>
+            <Button
+              loading={loading}
+              type="primary"
+              size="large"
+              htmlType="submit"
+            >
+              Log In
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
 export default LoginView;
